c45987f2fcfc03bde03b8f2c65f4e118bbc6842d
Perry Brandiezs <pbrandiezs@gmail.com>
2018-12-17 18:49:35 -0800

    Added cast_list.py to parse Monty Python actors. Added additional example in camelot_example.py.

---
+++ camelot_example.py
@@ -0,0 +1,14 @@
+# Example of specifying the number of characters to read
+with open("camelot.txt") as song:
+    print(song.read(2))
+    print(song.read(8))
+    print(song.read())
+
+
+#this example strips by line
+camelot_lines = []
+with open("camelot.txt") as f:
+    for line in f:
+        camelot_lines.append(line.strip())
+
+print(camelot_lines)
