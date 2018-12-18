485521031b86d6c22bb9ea45eb09eaada8bbe990
Perry Brandiezs <pbrandiezs@gmail.com>
2018-12-17 18:22:51 -0800

    Modified too_many_files.py to close open files.

--- too_many_files.py
+++ too_many_files.py
@@ -1,5 +1,8 @@
 # This script will generate an error due to too many files open.
 files = []
 for i in range(10000):
-    files.append(open('some_file.txt', 'r'))
+#     files.append(open('some_file.txt', 'r'))
+    f = open('some_file.txt', 'r')
+    f.close()
+    files.append(f)
     print(i)
